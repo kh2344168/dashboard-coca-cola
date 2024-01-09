0473ca20c07d7e61c8646bec99daebac1b79259f
kh2344168 <kh2344168@gmail.com>
2024-01-09 18:06:06 +0200

    fkdj

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidebarComponent } from './dashboard/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dashboard/sidebar/sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent {
+  sidebarItems = [
+    { icon: '../../../assets/Dashboard.png', label: 'dashboard', active: true },
+    { icon: '../../../assets/machine.png', label: 'machine', active: false },
+    { icon: '../../../assets/planning.png', label: 'planning', active: false },
+    { icon: '../../../assets/reports.png', label: 'reports', active: false }
+  ];
+}
